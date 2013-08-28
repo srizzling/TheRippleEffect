ea1d436760aee8c9e80500e6f6661802ad1b128d
Sivan <fesherman@gmail.com>
2013-08-29 11:31:27 +1200

    Just messing with things

--- wainz/main/urls.py
+++ wainz/main/urls.py
@@ -83,7 +83,7 @@ urlpatterns += staticfiles_urlpatterns()
 # CMS
 if settings.DEBUG:
     urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^media2/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'', include('django.contrib.staticfiles.urls')),
 ) + urlpatterns
